0bcc39ef3b31c2a5406e3c1d0de675305a55061f
Abraham <acv81505@lasallebajio.edu.mx>
2026-05-04 19:18:24 -0600

    Version final del juego 1

--- src/main.cpp
+++ src/main.cpp
@@ -1,41 +1,57 @@
 #include <Arduino.h>
 
-// Definimos nuestros pines
 const int pinIzq = 12;
 const int pinDer = 13;
 const int pinDisp = 14;
 const int ledPin = 2; 
 
+const int umbral = 90; 
+
+unsigned long tiempoGolpe = 0;
+bool recibioGolpe = false;
+
 void setup() {
   Serial.begin(115200);
-  delay(1000);
-  
-  // Configuramos el LED como salida
   pinMode(ledPin, OUTPUT);
-  Serial.println("--- Iniciando monitor de sensores Touch ---");
+  analogWrite(ledPin, 0); 
 }
 
 void loop() {
-  // Leemos los valores de los 3 sensores
   int valIzq = touchRead(pinIzq);
   int valDer = touchRead(pinDer);
   int valDisp = touchRead(pinDisp);
 
-  // Imprimimos los valores para verlos en tiempo real
-  Serial.print("Izq (12): "); Serial.print(valIzq);
-  Serial.print(" | Der (13): "); Serial.print(valDer);
-  Serial.print(" | Disparo (14): "); Serial.println(valDisp);
-
-  // --- PRUEBA DE PWM Y FEEDBACK ---
-  // Si tocamos el cable de disparo (el valor baja de 30), encendemos el LED con PWM
-  if (valDisp < 30) { 
-    // analogWrite manda una señal PWM (0 a 255). 
-    // 10 = Brillo muy tenue, 127 = Brillo medio, 255 = Brillo máximo
-    analogWrite(ledPin, 10); 
-  } else {
-    analogWrite(ledPin, 0); // Apagamos el LED si no tocamos
+  if (valIzq < umbral) {
+    Serial.println("I");
+  }
+  if (valDer < umbral) {
+    Serial.println("D");
+  }
+  if (valDisp < umbral) {
+    Serial.println("A");
   }
 
-  // Pequeña pausa para poder leer la terminal sin que vuelva loco a VS Code
-  delay(200); 
+  if (Serial.available() > 0) {
+    String comando = Serial.readStringUntil('\n');
+    comando.trim();
+    if (comando == "HIT") {
+      recibioGolpe = true;
+      tiempoGolpe = millis();
+    }
+  }
+
+
+  if (recibioGolpe) {
+    unsigned long tiempoPasado = millis() - tiempoGolpe;
+    
+    if (tiempoPasado < 500) {
+      int intensidad = (tiempoPasado / 50) % 2 == 0 ? 255 : 50; 
+      analogWrite(ledPin, intensidad);
+    } else {
+      recibioGolpe = false; 
+      analogWrite(ledPin, 0); 
+    }
+  }
+
+  delay(30);
 }
